947bdd77b5a30a64fa62648f726140064f8e0af0
Quentin DHERSIN <q.dhersin@everwin.fr>
2021-08-17 16:25:27 +0200

    'Test'

---
+++ naiveBot.py
@@ -0,0 +1,39 @@
+# Importing chatterbot
+from chatterbot import ChatBot
+
+# Create object of ChatBot class with Logic Adapter
+bot = ChatBot(
+    'Chappie',  
+    logic_adapters=[
+        'chatterbot.logic.BestMatch',
+        'chatterbot.logic.TimeLogicAdapter'],
+)
+
+# Inport ListTrainer
+from chatterbot.trainers import ListTrainer
+
+trainer = ListTrainer(bot)
+
+trainer.train([
+'Hi',
+'Hello',
+'I need your assistance regarding my order',
+'Please, Provide me with your order id',
+'I have a complaint.',
+'Please elaborate, your concern',
+'How long it will take to receive an order ?',
+'An order takes 3-5 Business days to get delivered.',
+'Okay Thanks',
+'No Problem! Have a Good Day!'
+])
+
+name=input("Enter Your Name: ")
+print("Welcome to the Bot Service! Let me know how can I help you?")
+while True:
+    request=input(name+':')
+    if request=='Bye' or request =='bye':
+        print('Bot: Bye')
+        break
+    else:
+        response=bot.get_response(request)
+        print('Bot:',response)
